d7483641f30da1cf4107d36501f28c819d0f651f
bhanuteja1110 <bhanuteja0985@gmail.com>
2026-01-31 23:22:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ window.onload = () => {
   setTimeout(() => {
     document.getElementById("popup").style.display = "flex";
   }, 3000);
-}
+};
 
 function closePopup(){
   document.getElementById("popup").style.display = "none";
 }
+
